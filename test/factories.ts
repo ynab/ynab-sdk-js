427dee6519660bb47ee2f081d64d2680ea562fcc
Taylor Brown <taylor@youneedabudget.com>
2017-11-22 14:08:41 -0600

    DRY up tests and add more tests
    Also, fix the swagger spec to be the latest one.

---
+++ test/factories.ts
@@ -0,0 +1,109 @@
+import * as Factory from "./factory";
+import * as api from "../src/api";
+
+export const budgetSummaryFactory = Factory.makeFactory<api.BudgetSummary>({
+  id: Factory.each(i => `${i}`),
+  name: Factory.each(i => `Budget ${i}`),
+  last_accessed_on: new Date().toISOString(),
+  date_format: { locale: "en-us" },
+  currency_format: { locale: "en-us" }
+});
+
+export const budgetSummaryResponseFactory = Factory.makeFactory<
+  api.BudgetSummaryResponse
+>({
+  data: {
+    budgets: Factory.makeFactory<api.BudgetSummary>({
+      id: Factory.each(i => `${i}`),
+      name: Factory.each(i => `Budget ${i}`),
+      last_accessed_on: new Date().toISOString(),
+      date_format: { locale: "en-us" },
+      currency_format: { locale: "en-us" }
+    }).buildList(3)
+  }
+});
+
+export const budgetDetailResponseFactory = Factory.makeFactory<
+  api.BudgetDetailResponse
+>({
+  data: Factory.makeFactory({
+    server_knowledge: Factory.each(i => i),
+    budget: Factory.makeFactory<api.BudgetDetail>({
+      id: Factory.each(i => `${i}`),
+      name: Factory.each(i => `Budget ${i}`),
+      last_accessed_on: new Date().toISOString(),
+      date_format: { locale: "en-us" },
+      currency_format: { locale: "en-us" },
+      accounts: [], //Array<Account>,
+      payees: [], //Array<Payee>,
+      payee_locations: [], //Array<PayeeLocation>,
+      category_groups: [], //Array<CategoryGroup>,
+      categories: [], //Array<Category>,
+      months: [], //Array<MonthDetail>,
+      transactions: [], //Array<TransactionSummary>,
+      subtransactions: [], //Array<SubTransaction>,
+      scheduled_transactions: [], //Array<ScheduledTransactionSummary>,
+      scheduled_subtransactions: [] //Array<ScheduledSubTransaction>,
+    })
+  })
+});
+
+export const accountFactory = Factory.makeFactory({
+  id: Factory.each(i => `AccountID${i}`),
+  name: Factory.each(i => `Account ${i}`),
+  type: "checking",
+  on_budget: true,
+  closed: false,
+  note: Factory.each(i => `Note #${i}`),
+  balance: 5000
+});
+
+export const accountsResponseFactory = Factory.makeFactory<
+  api.AccountsResponse
+>({
+  data: Factory.makeFactory({
+    accounts: accountFactory.buildList(5)
+  })
+});
+
+export const accountResponseFactory = Factory.makeFactory({
+  data: Factory.makeFactory<api.AccountWrapper>({
+    account: accountFactory.build()
+  })
+});
+
+export const categoryFactory = Factory.makeFactory<api.Category>({
+  id: Factory.each(i => `ID#${i}`),
+  name: Factory.each(i => `Name ${i}`),
+  category_group_id: Factory.each(i => `CategoryGroupID#${i}`),
+  hidden: false,
+  note: Factory.each(i => `Note#${i}`),
+  budgeted: Factory.each(i => i * 1000),
+  activity: Factory.each(i => i * 1000),
+  balance: Factory.each(i => i * 1000)
+});
+
+export const categoryGroupWithCategoriesFactory = Factory.makeFactory<
+  api.CategoryGroupWithCategories
+>({
+  id: Factory.each(i => `ID#${i}`),
+  name: Factory.each(i => `Name ${i}`),
+  hidden: false,
+  categories: categoryFactory.buildList(3)
+});
+
+export const categoriesResponseFactory = Factory.makeFactory<
+  api.CategoriesResponse
+>({
+  data: Factory.makeFactory({
+    category_groups: categoryGroupWithCategoriesFactory.buildList(3)
+  })
+});
+
+export const categoryResponseFactory = Factory.makeFactory<
+  api.CategoryResponse
+>({
+  data: Factory.makeFactory({
+    category: categoryFactory.build()
+  })
+});
--- test/mockTests.ts
+++ test/mockTests.ts
@@ -12,88 +12,42 @@ chai.config.includeStack = true;
 // We've made some modifications to factory.ts
 import * as Factory from "./factory";
 import * as api from "../src/api";
+import * as factories from "./factories";
 
 import * as fetchMock from "fetch-mock";
 
 const BASE_URL = "http://localhost:3000/papi/v1";
 
-const budgetSummaryFactory = Factory.makeFactory<api.BudgetSummary>({
-  id: Factory.each(i => `${i}`),
-  name: Factory.each(i => `Budget ${i}`),
-  last_accessed_on: new Date().toISOString(),
-  date_format: { locale: "en-us" },
-  currency_format: { locale: "en-us" }
-})
-
 // TODO: I'd like to be able to extend the budgetSummaryFactory with another typed factory
 // That would make it easier to make the budgetSummaryResponse for example
 
-const budgetSummaryResponseFactory = Factory.makeFactory<api.BudgetSummaryResponse>({
-  data: {
-    budgets: Factory.makeFactory<api.BudgetSummary>({
-      id: Factory.each(i => `${i}`),
-      name: Factory.each(i => `Budget ${i}`),
-      last_accessed_on: new Date().toISOString(),
-      date_format: { locale: "en-us" },
-      currency_format: { locale: "en-us" }
-    }).buildList(3)
-  }
-});
-
-const budgetDetailResponseFactory = Factory.makeFactory<
-  api.BudgetDetailResponse
->({
-  data: Factory.makeFactory({
-    server_knowledge: Factory.each(i => i),
-    budget: Factory.makeFactory<api.BudgetDetail>({
-      id: Factory.each(i => `${i}`),
-      name: Factory.each(i => `Budget ${i}`),
-      last_accessed_on: new Date().toISOString(),
-      date_format: { locale: "en-us" },
-      currency_format: { locale: "en-us" },
-      accounts: [], //Array<Account>,
-      payees: [], //Array<Payee>,
-      payee_locations: [], //Array<PayeeLocation>,
-      category_groups: [], //Array<CategoryGroup>,
-      categories: [], //Array<Category>,
-      months: [], //Array<MonthDetail>,
-      transactions: [], //Array<TransactionSummary>,
-      subtransactions: [], //Array<SubTransaction>,
-      scheduled_transactions: [], //Array<ScheduledTransactionSummary>,
-      scheduled_subtransactions: [] //Array<ScheduledSubTransaction>,
-    })
-  })
-});
-
 // You can get your API key from the My Account section of YNAB
 const API_KEY = "API_KEY";
-
-before(async () => {});
 
 beforeEach(async () => {
   fetchMock.reset();
   fetchMock.restore();
 });
 
+after(() => {
+  fetchMock.reset();
+  fetchMock.restore();
+});
+
 describe("Mock tests", () => {
   it("Should getBudgets and validate the request is sent correctly", async () => {
-    const mockResponse = budgetSummaryResponseFactory.build();
+    const mockResponse = factories.budgetSummaryResponseFactory.build();
     const ynab: ynabApi = new ynabApi(API_KEY, BASE_URL);
+
     const getBudgetsResponse = await callApiAndVerifyResponse(
       () => ynab.budgets.getBudgets(0),
       mockResponse
     );
-
-    expect(fetchMock.calls().matched.length, "fetchMock calls").to.equal(1);
-    expect(fetchMock.lastUrl()).to.equal(`${BASE_URL}/budgets`);
-    expect(fetchMock.lastOptions().method).to.equal("GET");
-    expect(fetchMock.lastOptions().headers).to.deep.equal({
-      Authorization: "Bearer API_KEY"
-    });
+    verifyRequestDetails(`${BASE_URL}/budgets`);
   });
 
   it("Should getBudgetContents and validate the request is sent correctly", async () => {
-    const mockResponse = budgetDetailResponseFactory.build();
+    const mockResponse = factories.budgetDetailResponseFactory.build();
     const ynab: ynabApi = new ynabApi(API_KEY, BASE_URL);
 
     const budgetId = "1234";
@@ -102,34 +56,15 @@ describe("Mock tests", () => {
       () => ynab.budgets.getBudgetContents(budgetId, lastKnowledgeOfServer),
       mockResponse
     );
-
-    expect(fetchMock.calls().matched.length, "fetchMock calls").to.equal(1);
-    expect(fetchMock.lastUrl()).to.equal(
+    verifyRequestDetails(
       `${BASE_URL}/budgets/${budgetId}?last_knowledge_of_server=${
         lastKnowledgeOfServer
       }`
     );
-    expect(fetchMock.lastOptions().method).to.equal("GET");
-    expect(fetchMock.lastOptions().headers).to.deep.equal({
-      Authorization: "Bearer API_KEY"
-    });
   });
 
   it("Should getAccounts and validate the request is sent correctly", async () => {
-    const mockResponse = Factory.makeFactory<api.AccountsResponse>({
-      data: Factory.makeFactory({
-        accounts: Factory.makeFactory({
-          id: Factory.each(i => `AccountID${i}`),
-          name: Factory.each(i => `Account ${i}`),
-          type: "checking",
-          on_budget: true,
-          closed: false,
-          note: Factory.each(i => `Note #${i}`),
-          balance: 5000
-        }).buildList(5)
-      })
-    }).build();
-
+    const mockResponse = factories.accountsResponseFactory.build();
     const ynab: ynabApi = new ynabApi(API_KEY, BASE_URL);
 
     const budgetId = "1234";
@@ -137,46 +72,98 @@ describe("Mock tests", () => {
       () => ynab.accounts.getAccounts(budgetId),
       mockResponse
     );
-
-    expect(fetchMock.calls().matched.length, "fetchMock calls").to.equal(1);
-    expect(fetchMock.lastUrl()).to.equal(
-      `${BASE_URL}/budgets/${budgetId}/accounts`
-    );
-    expect(fetchMock.lastOptions().method).to.equal("GET");
-    expect(fetchMock.lastOptions().headers).to.deep.equal({
-      Authorization: "Bearer API_KEY"
-    });
+    verifyRequestDetails(`${BASE_URL}/budgets/${budgetId}/accounts`);
   });
 
-  async function callApiAndVerifyResponse<ResponseType>(
-    promiseFunc: () => Promise<ResponseType>,
-    mockResponse: ResponseType
-  ) {
-    fetchMock.once("*", mockResponse);
-    let actualResponse = await promiseFunc();
-    expect(actualResponse).to.deep.equal(mockResponse);
-    return actualResponse;
-  }
+  it("Should getAccountById and validate the request is sent correctly", async () => {
+    const mockResponse = factories.accountResponseFactory.build();
+    const ynab: ynabApi = new ynabApi(API_KEY, BASE_URL);
 
-  function verifyFetchCalls(
-    numCalls: number,
-    url: string,
-    method: "GET" | "POST" | "PUT" | "PATCH",
-    bearerTokenExpected: string = null
-  ) {
-    expect(fetchMock.calls().matched.length, "fetchMock calls").to.equal(
-      numCalls
+    const budgetId = "1234";
+    const accountId = "DummyAccountId";
+    const returnedResponse = await callApiAndVerifyResponse(
+      () => ynab.accounts.getAccountById(budgetId, accountId),
+      mockResponse
     );
-    expect(fetchMock.lastUrl()).to.equal(url);
-    expect(fetchMock.lastOptions().method).to.equal(method);
-    const headers = fetchMock.lastOptions().headers;
-    if (bearerTokenExpected) {
-      // Should we deep equals, or just check the Authorization header?
-      expect((<any>headers)["Authorization"]).to.equal(
-        `Bearer ${bearerTokenExpected}`
-      );
-    } else {
-      expect((<any>headers)["Authorization"]).to.be.undefined;
-    }
-  }
+    verifyRequestDetails(
+      `${BASE_URL}/budgets/${budgetId}/accounts/${accountId}`
+    );
+  });
+
+  it("Should escape Ids in the URL", async () => {
+    const mockResponse = factories.accountResponseFactory.build();
+    const ynab: ynabApi = new ynabApi(API_KEY, BASE_URL);
+
+    const budgetId = "/://{}?";
+    const accountId = "/://{}?";
+    const returnedResponse = await callApiAndVerifyResponse(
+      () => ynab.accounts.getAccountById(budgetId, accountId),
+      mockResponse
+    );
+    verifyRequestDetails(
+      `${BASE_URL}/budgets/${encodeURIComponent(
+        budgetId
+      )}/accounts/${encodeURIComponent(accountId)}`
+    );
+  });
+
+  it("Should getCategories and validate the request is sent correctly", async () => {
+    const mockResponse = factories.categoriesResponseFactory.build();
+    const ynab: ynabApi = new ynabApi(API_KEY, BASE_URL);
+
+    const budgetId = "1234";
+    const accountId = "DummyAccountId";
+    const returnedResponse = await callApiAndVerifyResponse(
+      () => ynab.categories.getCategories(budgetId),
+      mockResponse
+    );
+    verifyRequestDetails(`${BASE_URL}/budgets/${budgetId}/categories`);
+  });
+
+  it("Should getCategoryById and validate the request is sent correctly", async () => {
+    const mockResponse = factories.categoryResponseFactory.build();
+    const ynab: ynabApi = new ynabApi(API_KEY, BASE_URL);
+
+    const budgetId = "1234";
+    const categoryId = "DummyCategoryId";
+    const returnedResponse = await callApiAndVerifyResponse(
+      () => ynab.categories.getCategoryById(budgetId, categoryId),
+      mockResponse
+    );
+    verifyRequestDetails(
+      `${BASE_URL}/budgets/${budgetId}/categories/${categoryId}`
+    );
+  });
 });
+
+async function callApiAndVerifyResponse<ResponseType>(
+  promiseFunc: () => Promise<ResponseType>,
+  mockResponse: ResponseType
+) {
+  fetchMock.once("*", mockResponse);
+  let actualResponse = await promiseFunc();
+  expect(actualResponse).to.deep.equal(mockResponse);
+  return actualResponse;
+}
+
+function verifyRequestDetails(
+  url: string,
+  bearerTokenExpected: string = API_KEY,
+  numCalls: number = 1,
+  method: "GET" | "POST" | "PUT" | "PATCH" = "GET"
+) {
+  expect(fetchMock.calls().matched.length, "fetchMock calls").to.equal(
+    numCalls
+  );
+  expect(fetchMock.lastUrl()).to.equal(url);
+  expect(fetchMock.lastOptions().method).to.equal(method);
+  const headers = fetchMock.lastOptions().headers;
+  if (bearerTokenExpected) {
+    // Should we deep equals, or just check the Authorization header?
+    expect((<any>headers)["Authorization"]).to.equal(
+      `Bearer ${bearerTokenExpected}`
+    );
+  } else {
+    expect((<any>headers)["Authorization"]).to.be.undefined;
+  }
+}
