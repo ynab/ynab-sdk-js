2830a800403d9db32336e34536fc804e260d8415
Brady Holt <brady.holt@gmail.com>
2017-11-25 11:51:44 -0600

    Put v1 endpoint in the template

--- src/api.ts
+++ src/api.ts
@@ -17,7 +17,7 @@ import * as url from "url";
 import * as portableFetch from "portable-fetch";
 import { Configuration } from "./configuration";
 
-const BASE_PATH = "https://./papi/v1".replace(/\/+$/, "");
+const BASE_PATH = "https://api.youneedabudget.com/v1/";
 
 /**
  *
@@ -40,7 +40,7 @@ export interface FetchAPI {
 }
 
 /**
- *  
+ *
  * @export
  * @interface FetchArgs
  */
@@ -50,7 +50,7 @@ export interface FetchArgs {
 }
 
 /**
- * 
+ *
  * @export
  * @class BaseAPI
  */
@@ -66,7 +66,7 @@ export class BaseAPI {
 };
 
 /**
- * 
+ *
  * @export
  * @class RequiredError
  * @extends {Error}
