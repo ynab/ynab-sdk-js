13343ae59c6308a1e287ec4164b45a104547f26a
Kyle Robinson Young <kyle@dontkry.com>
2017-12-27 13:00:08 -0800

    Default to 2 decimal digits when omitted from convertMilliUnitsToCurrencyAmount

--- test/utils.ts
+++ test/utils.ts
@@ -5,50 +5,44 @@ const assert = chai.assert;
 const expect = chai.expect;
 
 describe("utiilities", () => {
-  const utils = new Utils();
+    const utils = new Utils();
 
-  describe("getTodaysDateInISOFormat", () => {
-    it("Returns today's date in ISO format", () => {
-      let now = new Date();
-      let month = ("0" + (now.getMonth() + 1)).slice(-2);
-      let today = now.getFullYear() + "-" + month + "-01";
+    describe("getTodaysDateInISOFormat", () => {
+        it("Returns today's date in ISO format", () => {
+            let now = new Date();
+            let month = ("0" + (now.getMonth() + 1)).slice(-2);
+            let today = now.getFullYear() + "-" + month + "-01";
 
-      expect(utils.getCurrentMonthInISOFormat()).to.equal(today);
-    });
-  });
-
-  describe("convertFromISODateString", () => {
-    it("Converts an ISO formatted date string to a JS date object", () => {
-      let date = "2015-12-30";
-      expect(utils.convertFromISODateString(date).getTime()).to.equal(
-        Date.parse(date)
-      );
-    });
-  });
-
-  describe("convertMilliUnitsToCurrencyAmount", () => {
-    it("Converts millidolars to 2 decimimal currency", () => {
-      expect(utils.convertMilliUnitsToCurrencyAmount(239323, 2)).to.equal(
-        239.32
-      );
+            expect(utils.getCurrentMonthInISOFormat()).to.equal(today);
+        });
     });
 
-    it("Rounds up", () => {
-      expect(utils.convertMilliUnitsToCurrencyAmount(239325, 2)).to.equal(
-        239.33
-      );
+    describe("convertFromISODateString", () => {
+        it("Converts an ISO formatted date string to a JS date object", () => {
+            let date = "2015-12-30";
+            expect(utils.convertFromISODateString(date).getTime()).to.equal(Date.parse(date));
+        });
     });
 
-    it("Converts millidolars to 3 decimal currency", () => {
-      expect(utils.convertMilliUnitsToCurrencyAmount(239323, 3)).to.equal(
-        239.323
-      );
-    });
+    describe("convertMilliUnitsToCurrencyAmount", () => {
+        it("Converts millidolars to 2 decimimal currency", () => {
+            expect(utils.convertMilliUnitsToCurrencyAmount(239323, 2)).to.equal(239.32);
+        });
 
-    it("Converts millidolars to 0 decimal currency", () => {
-      expect(utils.convertMilliUnitsToCurrencyAmount(239323, 0)).to.equal(
-        239
-      );
+        it("Rounds up", () => {
+            expect(utils.convertMilliUnitsToCurrencyAmount(239325, 2)).to.equal(239.33);
+        });
+
+        it("Converts millidolars to 3 decimal currency", () => {
+            expect(utils.convertMilliUnitsToCurrencyAmount(239323, 3)).to.equal(239.323);
+        });
+
+        it("Converts millidolars to 0 decimal currency", () => {
+            expect(utils.convertMilliUnitsToCurrencyAmount(239323, 0)).to.equal(239);
+        });
+
+        it("Defaults to 2 decimal places when omitted", () => {
+            expect(utils.convertMilliUnitsToCurrencyAmount(239323)).to.equal(239.32);
+        });
     });
-  });
 });
