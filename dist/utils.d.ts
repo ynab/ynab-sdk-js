372d9b5012749fb35f93d3f1c30dddd124651863
Brady Holt <brady.holt@gmail.com>
2018-03-07 11:40:45 -0600

    Build dist/

--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -1,9 +1,7 @@
-import * as CodeGen from "./api";
 declare const _default: {
     getCurrentMonthInISOFormat(): string;
     getCurrentDateInISOFormat(): string;
     convertFromISODateString(isoDateString: string): Date;
     convertMilliUnitsToCurrencyAmount(milliunits: number, currencyDecimalDigits?: number): number;
-    convertTransactionToSaveTransaction(transaction: CodeGen.TransactionDetail): CodeGen.SaveTransaction;
 };
 export default _default;
