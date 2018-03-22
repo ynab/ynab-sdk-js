3c51b0e76b45c1535856ec073f834ebcc740c771
Brady Holt <brady.holt@gmail.com>
2018-03-22 09:57:08 -0500

    Set flag_color to show how to use enums

--- examples/update-transaction/index.ts
+++ examples/update-transaction/index.ts
@@ -13,6 +13,7 @@ const transactionId = "8fdf39f9-8f62-4efe-8dd2-64cb167ac6da";
   )).data.transaction;
 
   transaction.memo = "Updated memo";
+  transaction.flag_color = ynab.SaveTransaction.FlagColorEnum.Blue;
 
   await ynabAPI.transactions.updateTransaction(budgetId, transactionId, {
     transaction
