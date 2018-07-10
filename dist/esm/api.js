b8729026cf697d8e1f78e1078fcb0fd97a9767e8
Brady Holt <brady.holt@gmail.com>
2018-07-10 11:10:53 -0500

    Build structure tweaks for consistency w/ Ruby client and ease of use

--- dist/esm/api.js
+++ dist/esm/api.js
@@ -24,7 +24,7 @@ import * as url from "url";
 // Requiring portable-fetch like this ensures that we have a global fetch function
 // That makes it easier to override with modules like fetch-mock
 require("portable-fetch");
-var USER_AGENT = "api_client/js/1.0.0";
+var USER_AGENT = "api_client/js/1.1.0";
 function convertDateToFullDateStringFormat(date) {
     // Convert to RFC 3339 "full-date" format, like "2017-11-27"
     if (date instanceof Date) {
