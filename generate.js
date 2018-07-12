cbfe7e11414ff39f6ab74f86bc646ab543c1fd78
Brady Holt <brady.holt@gmail.com>
2018-07-12 16:39:19 -0500

    Generate markdown docs

--- generate.js
+++ generate.js
@@ -41,8 +41,6 @@ exec(`docker run --rm -v ${rootFolder}:/local swaggerapi/swagger-codegen-cli gen
 // Since the typescript-fetch generate does not support docs, we will also generate using the javascript generator
 // just so we can get the docs and move them over to doc/
 exec(`docker run --rm -v ${rootFolder}:/local swaggerapi/swagger-codegen-cli generate \
-      -DmodelPropertyNaming=original \
-      --type-mappings DateTime=string \
       -i "/local/${specFilename}" \
       -l "javascript" \
       -c "/local/${swaggerConfigFilename}" \
