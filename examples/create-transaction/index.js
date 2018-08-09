800df0a1b94f236e8c4700e606411b90733890da
Brady Holt <brady.holt@gmail.com>
2018-08-09 14:40:50 -0500

    Add transaction creation example

---
+++ examples/create-transaction/index.js
@@ -0,0 +1,19 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+const ynab = require("../../dist/index.js");
+const accessToken = "ccbb2db8-7c1b-not-real-b755-784876927790";
+const ynabAPI = new ynab.API(accessToken);
+const budgetId = "f968197b-2863-473a-8974-c2406dbe7f0d";
+const transactionId = "8fdf39f9-8f62-4efe-8dd2-64cb167ac6da";
+(async function () {
+    try {
+        const transaction = (await ynabAPI.transactions.getTransactionsById(budgetId, transactionId)).data.transaction;
+        transaction.memo = "Updated memo";
+        transaction.flag_color = ynab.SaveTransaction.FlagColorEnum.Blue;
+        await ynabAPI.transactions.updateTransaction(budgetId, transactionId, {
+            transaction
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
+})();
