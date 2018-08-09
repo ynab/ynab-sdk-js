c53fbdc7c5a44aa3651b205a6e8a31fabef79c31
Brady Holt <brady.holt@gmail.com>
2018-08-09 15:10:29 -0500

    Formatting

--- examples/create-transaction/index.js
+++ examples/create-transaction/index.js
@@ -4,17 +4,17 @@ const accessToken = "ccbb2db8-7c1b-not-real-b755-784876927790";
 const ynabAPI = new ynab.API(accessToken);
 const budgetId = "378f111e-4b27-4ee5-a9fd-0ba03c1de0f0";
 (async function () {
+    const transaction = {
+        account_id: "d99072b8-f598-484c-a0f8-ae607350f70a",
+        category_id: "8d505bc0-efbe-4803-b8ef-0898213a1092",
+        payee_id: null,
+        cleared: ynab.SaveTransaction.ClearedEnum.Cleared,
+        approved: true,
+        date: ynab.utils.getCurrentDateInISOFormat(),
+        amount: -23430,
+        memo: "Dry Cleaning"
+    };
     try {
-        const transaction = {
-            account_id: "d99072b8-f598-484c-a0f8-ae607350f70a",
-            category_id: "8d505bc0-efbe-4803-b8ef-0898213a1092",
-            payee_id: null,
-            cleared: ynab.SaveTransaction.ClearedEnum.Cleared,
-            approved: true,
-            date: ynab.utils.getCurrentDateInISOFormat(),
-            amount: -23430,
-            memo: "Dry Cleaning"
-        };
         await ynabAPI.transactions.createTransaction(budgetId, { transaction });
     }
     catch (err) {
