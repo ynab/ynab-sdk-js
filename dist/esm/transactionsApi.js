07ebd4657971493dd89fb4e10b11ddb001b310f1
Brady Holt <brady.holt@gmail.com>
2021-04-12 12:56:20 -0600

    Build dist/

--- dist/esm/transactionsApi.js
+++ dist/esm/transactionsApi.js
@@ -18,6 +18,19 @@ var TransactionsApi = /** @class */ (function (_super) {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     /**
+     * Returns budget transactions by type
+     * @summary List transactions
+     * @param {string} budget_id - The id of the budget. \"last-used\" can be used to specify the last used budget and \"default\" can be used if default budget selection is enabled (see: https://api.youneedabudget.com/#oauth-default-budget).
+     * @param {&#39;uncategorized&#39; | &#39;unapproved&#39;} [type] - If specified, only transactions of the specified type will be included. \"uncategorized\" and \"unapproved\" are currently supported.
+     * @param {number} [last_knowledge_of_server] - The starting server knowledge.  If provided, only entities that have changed since `last_knowledge_of_server` will be included.
+     * @param {*} [options] - Override http request options.
+     * @throws {RequiredError}
+     * @memberof TransactionsApi
+     */
+    TransactionsApi.prototype.getTransactionsByType = function (budget_id, type, last_knowledge_of_server, options) {
+        return CodeGen.TransactionsApiFp(this.configuration).getTransactions(budget_id, undefined, type, last_knowledge_of_server, options)();
+    };
+    /**
      * Creates multiple transactions. Provide a body containing a 'transactions' array, multiple transactions will be created.
      * @summary Create a single transaction or multiple transactions
      * @param {string} budget_id - The id of the budget (\"last-used\" can also be used to specify the last used budget)
--- dist/transactionsApi.js
+++ dist/transactionsApi.js
@@ -4,6 +4,19 @@ exports.TransactionsApi = void 0;
 const CodeGen = require("./api");
 class TransactionsApi extends CodeGen.TransactionsApi {
     /**
+     * Returns budget transactions by type
+     * @summary List transactions
+     * @param {string} budget_id - The id of the budget. \"last-used\" can be used to specify the last used budget and \"default\" can be used if default budget selection is enabled (see: https://api.youneedabudget.com/#oauth-default-budget).
+     * @param {&#39;uncategorized&#39; | &#39;unapproved&#39;} [type] - If specified, only transactions of the specified type will be included. \"uncategorized\" and \"unapproved\" are currently supported.
+     * @param {number} [last_knowledge_of_server] - The starting server knowledge.  If provided, only entities that have changed since `last_knowledge_of_server` will be included.
+     * @param {*} [options] - Override http request options.
+     * @throws {RequiredError}
+     * @memberof TransactionsApi
+     */
+    getTransactionsByType(budget_id, type, last_knowledge_of_server, options) {
+        return CodeGen.TransactionsApiFp(this.configuration).getTransactions(budget_id, undefined, type, last_knowledge_of_server, options)();
+    }
+    /**
      * Creates multiple transactions. Provide a body containing a 'transactions' array, multiple transactions will be created.
      * @summary Create a single transaction or multiple transactions
      * @param {string} budget_id - The id of the budget (\"last-used\" can also be used to specify the last used budget)
