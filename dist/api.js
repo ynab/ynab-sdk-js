b0f29c8bc2a43e10cd633bbeacc60b4f7dd6d86f
Brady Holt <brady.holt@gmail.com>
2021-04-14 11:35:02 -0600

    Build latest

--- dist/api.js
+++ dist/api.js
@@ -17,7 +17,7 @@ const url = require("url");
 // Requiring portable-fetch like this ensures that we have a global fetch function
 // That makes it easier to override with modules like fetch-mock
 require("portable-fetch");
-const USER_AGENT = "api_client/js/1.21.0";
+const USER_AGENT = "api_client/js/1.24.0";
 function convertDateToFullDateStringFormat(date) {
     // Convert to RFC 3339 "full-date" format, like "2017-11-27"
     if (date instanceof Date) {
