322f00de5e05b0d417df833a39e1d29c5a218b98
Brady Holt <brady.holt@gmail.com>
2023-11-07 13:04:42 -0600

    Remove 'Null: "null" from enums

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -9,4 +9,9 @@ const generatorConfigOptions = `modelPropertyNaming=original,useSingleRequestPar
 const generatorArgs = `-i ./open_api_spec.yaml -g \"typescript-fetch\" --type-mappings Date=string --additional-properties=${generatorConfigOptions} -t ./templates -o src`;
 exec(`openapi-generator generate ${generatorArgs}`);
 
+echo('Removing `Null: "null"` from enum definitions...');
+// The OpenAPI Generator adds `Null: "null"` to enum definitions when a `- null` option is present in the spec.
+// The generator does not provide a way to disable this behavior, so we will remove these items manually.
+exec(`find ./src/models -type f -exec sed -i '' "s/Null: 'null'//g" {} +`);
+
 echo.green("Success!");
